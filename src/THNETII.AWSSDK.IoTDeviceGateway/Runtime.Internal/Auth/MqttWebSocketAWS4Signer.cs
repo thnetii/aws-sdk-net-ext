47f21833dc2dc0f5a510ab2cb9b691b3e303e695
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-09-27 19:56:35 +0200

    IoT Device Gateway: Using AWS4 Pre-Signed URL Signer

--- src/THNETII.AWSSDK.IoTDeviceGateway/Runtime.Internal/Auth/MqttWebSocketAWS4Signer.cs
+++ src/THNETII.AWSSDK.IoTDeviceGateway/Runtime.Internal/Auth/MqttWebSocketAWS4Signer.cs
@@ -2,19 +2,17 @@
 using Amazon.Runtime.Internal;
 using Amazon.Runtime.Internal.Auth;
 using Amazon.Runtime.Internal.Util;
-using Amazon.Util;
 
 namespace Amazon.IoTDeviceGateway.Runtime.Internal.Auth
 {
-    public class MqttWebSocketAWS4Signer : AWS4Signer
+    public class MqttWebSocketAWS4Signer : AWS4PreSignedUrlSigner
     {
-        public MqttWebSocketAWS4Signer(bool signPayload = false) : base(signPayload) { }
+        public MqttWebSocketAWS4Signer() : base() { }
 
         public override void Sign(IRequest request, IClientConfig clientConfig, RequestMetrics metrics, string awsAccessKeyId, string awsSecretAccessKey)
         {
-            var signingResult = SignRequest(request, clientConfig, metrics, awsAccessKeyId, awsSecretAccessKey);
+            var signingResult = SignRequest(request, clientConfig, metrics, awsAccessKeyId, awsSecretAccessKey, clientConfig.RegionEndpointServiceName, overrideSigningRegion: null);
             request.AWS4SignerResult = signingResult;
-            request.Headers[HeaderKeys.AuthorizationHeader] = signingResult.ForAuthorizationHeader;
         }
     }
 }
