7915d7b63201b0ee215284b2881e21535de0d4e2
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-02-16 23:42:56 +0100

    Suppress warning about uninstatiated model classes
    Model classes in unit tests are instatiated using JSON serialization.

--- test/THNETII.AWSSDK.Extensions.Test/TestParameters/Models.cs
+++ test/THNETII.AWSSDK.Extensions.Test/TestParameters/Models.cs
@@ -2,6 +2,7 @@
 
 namespace Amazon.TestParameters
 {
+    [SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes")]
     internal class MicTestParameters
     {
         public string? Hostname { get; set; }
@@ -9,6 +10,7 @@ namespace Amazon.TestParameters
         public string? Password { get; set; }
     }
 
+    [SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes")]
     internal class MicManifest
     {
         public string? ApiGatewayRootUrl { get; set; }
@@ -16,6 +18,7 @@ namespace Amazon.TestParameters
     }
 
 
+    [SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes")]
     internal class MicMetadataManifest
     {
         public string? ApiKey { get; set; }
@@ -27,12 +30,14 @@ namespace Amazon.TestParameters
     }
 
 
+    [SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes")]
     [SuppressMessage("Style", "IDE1006: Naming Styles")]
     internal class Login
     {
         public Credentials? credentials { get; set; }
     }
 
+    [SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes")]
     [SuppressMessage("Style", "IDE1006: Naming Styles")]
     internal class Credentials
     {
